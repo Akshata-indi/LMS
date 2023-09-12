57031c07241052da95f6ff331ca46188f1d28903
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2023-09-12 18:48:53 +0530

    footer and modal configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import Home from "./Pages/home/Home"
+// import Home from "./Pages/home/Home"
+// import Footer from "./config-components/footer/Footer";
+import ReadModal from '../src/config-components/modal/ReadModal'
 
-
-import Header from "./config-components/header/Header";
-
-
-
+// import Header from "./config-components/header/Header";
 
 function App() {
   return (
   
     <>
-      <Home />
-
-      
-     
+      {/* <Home /> */}
+      {/* <Footer />  */}
+      <ReadModal />
     </>
 
   );
 }
-
 
 export default App;
 
--- src/config-components/footer/FooterComponent.js
+++ src/config-components/footer/FooterComponent.js
@@ -1,46 +1,47 @@
-// FooterComponent.js
 import React from 'react';
-import footerConfig from "./footerConfig.json"
-import "../../scss/style.scss";
-import { Container, Grid, Typography, Link, IconButton } from '@mui/material';
 import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa'; // Import the icons
-
+import footerConfig from '../footer/footerConfig.json'
 
 const FooterComponent = () => {
+//   const styles = FooterConfig.styles;
+    const styles = footerConfig.styles;
+
   return (
-    <div className="footer-container">
-      <Container className="footer" component="footer">
-        <Grid container>
-          <Grid item xs={12} sm={6} md={6}>
-            <div className="company-links">
-              <Typography variant="h6" className="company-name">
-                {footerConfig.companyName}
-              </Typography>
-              <ul>
-                {footerConfig.companyLinks.map((link, index) => (
-                  <li key={index}>
-                    <Link href={link.url} color="textSecondary" underline="none">
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6} className="social-media">
+    <div className={`footer-container bg-gray-100 pt-8 pb-14`}>
+      <div className="footer mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center">
+          <div className={`company-links font-sans flex items-center text-gray-500 text-base font-semibold space-x-6}`}>
+            <h6 className={`company-name font-sans text-xl font-semibold mr-10`}>
+              {footerConfig.companyName}
+            </h6>
+            <ul className="flex space-x-6">
+              {footerConfig.companyLinks.map((link, index) => (
+                <li key={index}>
+                  <a href={link.url} className={`hover:text-gray-700 font-semibold text-base transition duration-300`}>
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="social-media flex items-center space-x-4">
             {footerConfig.socialMedia.map((social, index) => (
-              <IconButton key={index} href={social.url} target="_blank" rel="noopener noreferrer">
+              <a
+                key={index}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`text-xl text-gray-500 hover:text-gray-700 transition duration-300`}
+              >
                 {social.icon === 'facebook' && <FaFacebook />}
                 {social.icon === 'twitter' && <FaTwitter />}
                 {social.icon === 'linkedin' && <FaLinkedin />}
-              </IconButton>
+              </a>
             ))}
-          </Grid>
-        </Grid>
-      </Container>
-
-    
-     </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
