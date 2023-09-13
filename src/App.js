6e95505c9bf6c51e0c42addfad9d6aabb8b2a1e7
Ushamnaik999 <naik.usha@infokalash.com>
2023-09-13 12:03:36 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-// import Home from "./Pages/home/Home"
-
-// import Header from "./config-components/header/Header";
-
-
-
-// import DynamicInputs from "./config-components/inputs/DynamicInputs";
-
-// import Footer from "./config-components/footer/Footer";
-import ReadModal from '../src/config-components/modal/ReadModal'
-
-
-// import Header from "./config-components/header/Header";
+import Home from "./Pages/home/Home"
 
 function App() {
   return (
-  <>
-    
-      {/* <Home /> */}
-
-
-{/* <DynamicInputs /> */}
-      
-     {/* <h2 className="text-center">Hello React</h2> */}
+      <>
+    <Home /> 
      </> 
-
-    </>
-
-
   );
 }
 
+  
 export default App;
 
