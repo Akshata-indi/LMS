033a7beb227ca292aee5b0b2b43c2c1d07a9883a
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2023-09-27 14:08:24 +0530

    login and signup form

--- src/config-components/logo/LogoConfig.js
+++ src/config-components/logo/LogoConfig.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
 const LogoConfig = () => (
-<<<<<<< HEAD
- 
+
   <img
     src={logo}
     className="w-11 min-w-8"
@@ -11,12 +10,3 @@ const LogoConfig = () => (
  
 );
 export default LogoConfig; 
-=======
-  <img
-    src={logo}
-    className="w-12 min-w-8"
-    alt="Logo "
-  />
-);
-export default LogoConfig;
->>>>>>> 3177ed7747b663cbf38b822ebfc55b68cfff1924
