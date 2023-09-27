6798b927049075e17853ae02496213c2a454a9cb
Ushamnaik999 <naik.usha@infokalash.com>
2023-09-27 17:44:47 +0530

    Body

--- src/config-components/logo/LogoConfig.js
+++ src/config-components/logo/LogoConfig.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
 const LogoConfig = () => (
-<<<<<<< HEAD
- 
+  
+  
   <img
     src={logo}
-    className="w-11 min-w-8"
+    className="w-11 min-w-9 mx-1"
     alt="Logo "
   />
  
-);
-export default LogoConfig; 
-=======
-  <img
-    src={logo}
-    className="w-12 min-w-8"
-    alt="Logo "
-  />
 );
 export default LogoConfig;
->>>>>>> 3177ed7747b663cbf38b822ebfc55b68cfff1924
+
+  
+
