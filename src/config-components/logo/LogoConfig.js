63c169c390f45a882028c18138cf91bc97b35e0a
Ushamnaik999 <naik.usha@infokalash.com>
2023-10-03 17:55:16 +0530

    header new

--- src/config-components/logo/LogoConfig.js
+++ src/config-components/logo/LogoConfig.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
 const LogoConfig = () => (
-<<<<<<< HEAD
-
-=======
->>>>>>> 59e4d85ec2e8d125fa80370c93e655198e47350e
   <img
     src={logo}
     className="w-11 min-w-9 mx-1"
     alt="Logo "
   />
-<<<<<<< HEAD
 
 );
 
 export default LogoConfig; 
-=======
- 
-header/add-header
-);
-Temporary merge branch 1
-export default LogoConfig; 
 
-); 
-
-export default LogoConfig;
->>>>>>> 59e4d85ec2e8d125fa80370c93e655198e47350e
 
