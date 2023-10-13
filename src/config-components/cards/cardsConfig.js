754208616e6f0f468641fb23aecb882080482fdc
Ushamnaik999 <naik.usha@infokalash.com>
2023-10-13 12:38:59 +0530

    Config Cards

--- src/config-components/cards/cardsConfig.js
+++ src/config-components/cards/cardsConfig.js
@@ -4,7 +4,7 @@ const cardsConfig = [
   
   {
     id: 'card2',
-    className: ' bg-white rounded-md border border-gray-300 shadow-lg', 
+    className: ' bg-white rounded-md border border-gray-300 shadow-md', 
   },
   
 ];
