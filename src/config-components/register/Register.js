4324bf908ec5192dca41dedeb1ab65d1833be0de
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2023-10-26 12:00:31 +0530

    password recovery

--- src/config-components/register/Register.js
+++ src/config-components/register/Register.js
@@ -2,16 +2,16 @@ import React from 'react';
 import RegisterForm from '../register/RegisterForm'
 
 
-function register() {
+function Register() {
   
   return (
     <div>
-      <div className="flex justify-center items-center mt-10 bg-white">
+      <div className="flex justify-center m-1 items-center bg-white">
         <RegisterForm />
-      </div>
+      </div> 
     </div>
   );
 }
 
-export default register;
+export default Register;
   
